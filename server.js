f0377a07e773129fbaf764cc19c8db8dcf7650a5
meliibarramedina-tech <meliibarramedina@gmail.com>
2026-06-15 21:14:24 -0600

    gmail config

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ if (!process.env.GMAIL_USER || !process.env.GMAIL_APP_PASS) {
 
 // Email config
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: 'gmail',
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS,
