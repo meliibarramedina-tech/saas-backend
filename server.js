8e7eaa13ebbd851c9610b68c8d086f4bbb08e245
meliibarramedina-tech <meliibarramedina@gmail.com>
2026-06-15 20:50:47 -0600

    Fix SMTP Gmail

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ if (!process.env.GMAIL_USER || !process.env.GMAIL_APP_PASS) {
 
 // Email config
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASS,
